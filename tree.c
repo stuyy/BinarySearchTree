92829d8f338731054c4d517dd0a536c83300ee7b
ansonfoong <afoong@albany.edu>
2018-12-05 23:46:44 -0500

    insertTree checks if data being added should go to the left or right.

--- tree.c
+++ tree.c
@@ -9,25 +9,48 @@ int isEmpty(NODE ** ROOT)
     else
         return 0;
 }
+// insertTree takes in a Double Pointer, which is the address of the pointer to the Root Node.
+// it takes in an integer, which will resemble the data we're trying to insert into the Tree.
 void insertTree(NODE ** ROOT, int data)
 {
-    if(*ROOT == NULL)
+    if(*ROOT == NULL) // Dereference the double pointer, to reference the address of the ROOT Node. 
         *ROOT = createNode(data);
+    else if((*ROOT)->data == data)
+        return;
     else {
-        
+        // Check if the data needs to go to the left or the right.
+        // If the data to add is less than the data to the left, then we need to recursively call insertTree, passing it the leftChild of the current Node.
+        if(data < (*ROOT)->data) 
+        {
+            // Check if the left Child is NULL.
+            if((*ROOT)->leftChild == NULL)
+                (*ROOT)->leftChild = createNode(data);
+            else
+                insertTree(&(*ROOT)->leftChild, data); // Pass in the address to the leftChild of the NODE Pointer.
+        }
+        else if(data > (*ROOT)->data)
+        {
+            if((*ROOT)->rightChild == NULL)
+                (*ROOT)->rightChild = createNode(data);
+            else
+                insertTree(&(*ROOT)->rightChild, data);
+        }
     }
-    
 }
 
 int main(void)
 {
     NODE * ROOT = NULL;
     insertTree(&ROOT, 4);
-    
+    insertTree(&ROOT, 2);
+    insertTree(&ROOT, 6);
+
     if(isEmpty(&ROOT))
         printf("The root is empty.\n");
     else {
         printf("The root is %d\n", ROOT->data);
+        printf("The root's left is %d\n", ROOT->leftChild->data);
+        printf("The root's right is %d\n", ROOT->rightChild->data);
     }
     return 0;
 }
