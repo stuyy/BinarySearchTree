4b39f1044915f45fcf2393495df33d0d7c99bd1a
ansonfoong <afoong@albany.edu>
2018-12-05 23:37:44 -0500

    Check if the Root is null inside insertTree, if it is, call createNode method to allocate memory for the node.

--- tree.c
+++ tree.c
@@ -2,17 +2,32 @@
 #include <stdlib.h>
 #include "node.c"
 
+int isEmpty(NODE ** ROOT)
+{
+    if(*ROOT == NULL)
+        return 1;
+    else
+        return 0;
+}
 void insertTree(NODE ** ROOT, int data)
 {
     if(*ROOT == NULL)
-    {
-        printf("The Root is Null.\n");
+        *ROOT = createNode(data);
+    else {
+        
     }
+    
 }
 
 int main(void)
 {
     NODE * ROOT = NULL;
     insertTree(&ROOT, 4);
+    
+    if(isEmpty(&ROOT))
+        printf("The root is empty.\n");
+    else {
+        printf("The root is %d\n", ROOT->data);
+    }
     return 0;
 }
